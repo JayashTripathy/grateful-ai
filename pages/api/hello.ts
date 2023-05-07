8df586861210687510599a0e08247b7988f4c5d3
JayashTripathy <jayashtripathy371@gmail.com>
2023-05-08 01:22:54 +0530

    setted up open AI api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,37 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from "next";
+import { OpenAIStream, OpenAIStreamPayload } from "../../utils/OpenAIStream"
 
-type Data = {
-  name: string
+
+if (!process.env.OPENAI_KEY) {
+  throw new Error("Missing env var from OpenAI")
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+
+
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  
+  const { prompt } = (await req.body) as {
+    prompt: string;
+  };
+
+  if (!prompt) {
+    return new Response("No prompt in the request", { status: 400 })
+  }
+
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Generate a sentence of gratitude for ${prompt}. Their pronouns are he/his. 
+    In a similar format to, I am grateful for ${prompt} because...`,
+    temperature: 0,
+    max_tokens: 100,
+  });
+
+  res.status(200).json(response.data)
 }
